96758218dc46efcd01fef2bb2a6cd0845b732ff4
Ayoub Driouich <adriouic@e2r2p5.1337.ma>
2021-11-15 18:03:21 +0100

    adding static var

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,26 @@
 /*   By: adriouic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 23:00:01 by adriouic          #+#    #+#             */
-/*   Updated: 2021/11/15 16:06:55 by adriouic         ###   ########.fr       */
+/*   Updated: 2021/11/15 18:01:52 by adriouic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#define BUFFER_SIZE 10000000
 
 char	*get_next_line(int fd)
-{
-	static char *s[1];
-	char		buffer[BUFFER_SIZE + 1];
+{	
+	static char	*s[1];
+	static char	buffer[BUFFER_SIZE + 1];
 	char		*temp;
 	int			rv;
-
+	
 	rv = read(fd, buffer, BUFFER_SIZE);
 	while (rv > 0)
 	{
+
 		buffer[rv] = '\0';
 		if (!s[0])
 			s[0] = ft_strdup(buffer);
@@ -37,22 +39,21 @@ char	*get_next_line(int fd)
 			break ;
 		rv = read(fd, buffer, BUFFER_SIZE);
 	}
-	if (rv <= 0)
-		return (s[0]);
-	return (ft_strip(s, BUFFER_SIZE));
+	if (rv < 0)
+		return (NULL);
+	return (ft_strip(s, BUFFER_SIZE, rv));
 }
-
-int main(void)
+int	main(void)
 {
 	int		fd;
 	int		j;
 	char	*res;
 
-	fd = open("test.txt", O_WRONLY);
-	if(fd < 0)
+	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
 		return (0);
 	j = 0;
-	while (j < 2)
+	while (j < 6)
 	{	
 		res = get_next_line(fd);
 		printf("<->%s\n", res);
